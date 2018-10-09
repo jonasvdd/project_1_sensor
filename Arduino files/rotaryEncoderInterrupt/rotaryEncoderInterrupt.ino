ef8c945af4d9d625d4428fb8d34074a0722a6eb3
mattijsl <mlootens@hotmail.com>
2018-10-09 11:21:24 +0200

    rotary encoder

--- Arduino files/rotaryEncoderInterrupt/rotaryEncoderInterrupt.ino
+++ Arduino files/rotaryEncoderInterrupt/rotaryEncoderInterrupt.ino
@@ -8,6 +8,7 @@
  #define outputA 2
  #define outputB 3
  int counter = 0; 
+ int angle = 0;
  int aState;
  int aLastState;  
  
@@ -18,14 +19,14 @@
    Serial.begin (9600);
    // Reads the initial state of the outputA
    aLastState = digitalRead(outputA);   
-   attachInterrupt(digitalPinToInterrupt(outputA), count, CHANGE);
-   attachInterrupt(digitalPinToInterrupt(outputB), count, CHANGE);
+   attachInterrupt(digitalPinToInterrupt(outputA), angle, CHANGE);
+   attachInterrupt(digitalPinToInterrupt(outputB), angle, CHANGE);
  } 
  
  void loop() { 
  }
 
- void count(){ aState = digitalRead(outputA); // Reads the "current" state of the outputA
+ void angle(){ aState = digitalRead(outputA); // Reads the "current" state of the outputA
    // If the previous and the current state of the outputA are different, that means a Pulse has occured
    if (aState != aLastState){     
      // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
@@ -34,9 +35,11 @@
      } else {
        counter --;
      }
+     counter = counter%40
      Serial.print("Position: ");
-     Serial.println(counter);
+     Serial.println(counter*9);
    } 
    aLastState = aState; // Updates the previous state of the outputA with the current state
+   angle =  counter*9;
  }
 
